17073d4b9e92b80812512854fa742504b9dc4e03
mansoh <manuelbarrososousa@gmail.com>
2026-05-09 22:39:44 +0100

    autenticacao por link magico

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 from .forms import RegistoForm
+from .models import Perfil
+import secrets
 
 def login_view(request):
     if request.method == 'POST':
@@ -27,3 +31,36 @@ def registo_view(request):
         return redirect('login')
     context = {'form': form}
     return render(request, 'accounts/registo.html', context)
+
+def envia_email(perfil, email):
+    send_mail(
+        subject='Portfolio: Autenticação',
+        message=f'Caro {perfil.user.first_name or perfil.user.username}, clique no link http://127.0.0.1:8000/accounts/autentica/?token={perfil.token} para entrar na aplicação.',
+        from_email='manuelbarrososousa@gmail.com',
+        recipient_list=[email]
+    )
+
+def login_magic_link(request):
+    email = request.GET.get('email')
+    if email:
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            perfil, _ = Perfil.objects.get_or_create(user=user)
+            perfil.token = secrets.token_urlsafe(32)
+            perfil.save()
+            envia_email(perfil, email)
+            return render(request, 'accounts/login.html', {'mensagem_info': 'Link enviado para o teu email.'})
+        else:
+            return render(request, 'accounts/login.html', {'mensagem': 'Email não encontrado.'})
+    return render(request, 'accounts/login.html')
+
+def autentica(request):
+    token = request.GET.get('token')
+    try:
+        perfil = Perfil.objects.get(token=token)
+        perfil.token = ''
+        perfil.save()
+        login(request, perfil.user)
+        return redirect('projetos')
+    except Perfil.DoesNotExist:
+        return render(request, 'accounts/login.html', {'mensagem': 'Link inválido ou expirado.'})
